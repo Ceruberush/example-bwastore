e777f4e2fe061bd34b94beb7c994ca9d9ffe5952
unknown <nugraha.permana07@gmail.com>
2021-08-10 17:30:57 +0900

    first commit

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request\Facades\Auth\Support;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {   
+        if(Auth::user() && Auth::user()->roles == 'ADMIN')
+        {
+            return $next($request);
+        }
+
+        return redirect('/');
+    }
+}
